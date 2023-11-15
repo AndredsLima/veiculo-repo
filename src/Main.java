6ff36909ee41728312ceff3af93d4fc440bc961c
BrenooNogg <breenonogg@gmail.com>
2023-11-15 16:15:04 -0300

    Chamada de método carro na classe principal

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
   public static void main(String[] args) throws Exception {
-System.out.println("Hello World ");
-
-
+    System.out.println("Hello World ");
+    Carro meuCarro = new Carro();
+    meuCarro.setCarroInfo("Toyota", "Touro", 2022);
+    meuCarro.getCarroInfo();
 
   }
 }
