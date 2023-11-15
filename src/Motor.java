2e607fce0c274794cc44d2e50378400c0c529094
BrenooNogg <breenonogg@gmail.com>
2023-11-15 15:51:59 -0300

    Criação classe Motor

---
+++ src/Motor.java
@@ -0,0 +1,54 @@
+// --------------Atributos----------------------
+public class Motor {
+    public String tipo;
+    public int potencia;
+    public boolean ligado;
+
+    // -------------------Construtor ---------------------------
+
+    public Motor(String tipo, int potencia) {
+
+        this.tipo = tipo;
+        this.potencia = potencia;
+        this.ligado = false;
+
+    }
+
+    // ----------------Métodos------------------------------
+    public void ligarMotor() {
+        ligado = true;
+        System.out.println("Ligando o Carro.... ");
+        System.out.println("Motor: " + getTipo() + " Potência: " + getPotencia() + " HP");
+
+    }
+
+    public void desligar() {
+        ligado = false;
+        System.out.println("Motor desligado !");
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setMotor(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+}
