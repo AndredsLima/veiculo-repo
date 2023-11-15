4b8a7a1ecfe945bdaae1a53b6b1b0394ab9f571b
André de Sousa Lima <andredslima01@gmail.com>
2023-11-15 16:46:08 -0300

    Chamada do metodo ligar Motor

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 public class Main {
-  public static void main(String[] args) throws Exception {
-    System.out.println("Hello World ");
-    Carro meuCarro = new Carro();
-    meuCarro.setCarroInfo("Toyota", "Touro", 2022);
-    meuCarro.getCarroInfo();
-    
-    meuCarro.percorrido(100);
-    
-    System.out.println("Quilometragem: " + meuCarro.getQuilometragem());
+    public static void main(String[] args) throws Exception {
+        System.out.println("Carro sem motor \n");
+        Carro meuCarro = new Carro();
+        meuCarro.setCarroInfo("Fiat", "Touro", 2022);
+        meuCarro.getCarroInfo();
 
-  }
+
+
+        System.out.println("Carro com motor \n");
+        Motor motorCarro = new Motor("v8", 1000);
+        Carro meuCarroComMotor = new Carro("Chevrolet", "Camaro", 2023, motorCarro);
+        
+        
+        meuCarroComMotor.ligar();
+
+        meuCarro.percorrido(100);
+        meuCarroComMotor.getCarroInfo();
+
+        System.out.println("Quilometragem: " + meuCarro.getQuilometragem());
+
+    }
 }
