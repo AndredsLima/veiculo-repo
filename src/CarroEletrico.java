c731c624431149871d1ab648362fe4b995598d2e
Antony <antony.caiquec@gmail.com>
2023-11-16 09:08:51 -0300

    Criação da classe Carro Elétrico

---
+++ src/CarroEletrico.java
@@ -0,0 +1,25 @@
+public class CarroEletrico extends Carro {
+    public int autonomiaBateria;
+
+    // ----------------Metodo para herdar-------------------------
+    @Override
+    public String toString() {
+        return "Marca: " + getMarca() + " , Modelo : " + getModelo() + " , Ano : " + getAno() + ", Autonomia de bateria: "
+                + getAutonomiaBateria();
+    }
+//-----------------------------------------------------------------
+
+
+    public int getAutonomiaBateria() {
+        return autonomiaBateria;
+    }
+
+
+    public void setAutonomiaBateria(int autonomiaBateria) {
+        this.autonomiaBateria = autonomiaBateria;
+    }
+    
+
+    
+
+}
